a6ed6f0e1cff7a49186b38bb83697d5032899a3a
totaler <totaler@gmail.com>
2013-05-07 18:29:13 +0200

    IMP Some PEP8 improvements

--- erppeek_wst.py
+++ erppeek_wst.py
@@ -8,20 +8,21 @@ import functools
 class ClientWST(Client):
 
     def __init__(self, server, db=None, user=None, password=None,
-             verbose=False):
+                 verbose=False):
         self.transaction_id = None
         methods = ['execute', 'get_transaction',
                    'commit', 'rollback', 'close']
         self._sync = Service(server, 'sync', methods, verbose=verbose)
         super(ClientWST,
               self).__init__(server, db=db, user=user,
-                        password=password, verbose=verbose)
+                             password=password, verbose=verbose)
 
     def login(self, user, password=None, database=None):
         super(ClientWST,
               self).login(user, password=password, database=database)
 
         args = self._execute.args
+
         def authenticated(method):
             return functools.partial(method, args[0], args[1], args[2])
         self._get_transaction = authenticated(self._sync.get_transaction)
