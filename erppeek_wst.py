596dc26b7024e146a80197fa9926dd4b550dbd82
David García Garzón <voki@canvoki.net>
2017-07-11 18:47:24 +0200

    changed to work with ws_transaction module

--- erppeek_wst.py
+++ erppeek_wst.py
@@ -11,8 +11,8 @@ class ClientWST(Client):
                  verbose=False):
         self.transaction_id = None
         methods = ['execute', 'get_transaction',
-                   'commit', 'rollback', 'close']
-        self._sync = Service(server, 'sync', methods, verbose=verbose)
+                   'commit', 'rollback', 'close', 'begin']
+        self._sync = Service(server, 'ws_transaction', methods, verbose=verbose)
         super(ClientWST,
               self).__init__(server, db=db, user=user,
                              password=password, verbose=verbose)
@@ -27,12 +27,21 @@ class ClientWST(Client):
             return functools.partial(method, args[0], args[1], args[2])
         self._get_transaction = authenticated(self._sync.get_transaction)
         self._execute_sync = authenticated(self._sync.execute)
+        self._execute_bare = self._execute
         self._commit = authenticated(self._sync.commit)
         self._rollback = authenticated(self._sync.rollback)
         self._close = authenticated(self._sync.close)
+        self._begin = authenticated(self._sync.begin)
 
     _login = login
     _login.cache = {}
+
+    def begin(self):
+        args = self._execute.args
+        tid = self._begin()
+        self.transaction_id = tid
+        self._execute = functools.partial(self._execute_sync, tid)
+        return self
 
     def get_transaction(self, transaction_id):
         self.transaction_id = self._get_transaction(transaction_id)
