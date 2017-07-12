808cd137e3fbdfb7efa3120f2cd559e42fce1efa
David García Garzón <voki@canvoki.net>
2017-07-12 23:47:36 +0200

    using an alias for close as Service overwrites it

--- erppeek_wst.py
+++ erppeek_wst.py
@@ -12,7 +12,7 @@ class ClientWST(Client):
                  verbose=False):
         self.transaction_id = None
         methods = ['execute', 'get_transaction',
-                   'commit', 'rollback', 'close', 'begin']
+                   'commit', 'rollback', 'close', 'begin', 'close_connection']
         self._sync = Service(server, 'ws_transaction', methods, verbose=verbose)
         super(ClientWST,
               self).__init__(server, db=db, user=user,
@@ -29,21 +29,18 @@ class ClientWST(Client):
         self._get_transaction = authenticated(self._sync.get_transaction)
         self._execute_bare = self._execute
         self._execute_sync = authenticated(self._sync.execute)
-        self._close_bare = authenticated(self._sync.close)
         self._commit = authenticated(self._sync.commit)
         self._rollback = authenticated(self._sync.rollback)
         self._begin = authenticated(self._sync.begin)
+        self._close_connection = authenticated(self._sync.close_connection)
 
     _login = login
     _login.cache = {}
 
     def begin(self):
-        args = self._execute.args
         tid = self._begin()
         self.transaction_id = tid
         self._execute = functools.partial(self._execute_sync, tid)
-        # we change it on the service as it is called directly by base
-        self._sync.close = functools.partial(self._close_bare, tid)
         return self
 
     def get_transaction(self, transaction_id):
@@ -64,5 +61,6 @@ class ClientWST(Client):
             if 'commands ignored until end of transaction block' not in str(e):
                 raise
 
-
+    def close(self):
+        self._close_connection(self.transaction_id)
 
