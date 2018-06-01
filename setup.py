f960695e2336524575286c37e0d63268b9554b78
Eduard Carreras <ecarreras@gisce.net>
2018-06-01 10:48:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='ERPpeek WST',
-    version=0.1,
+    version=0.2.0,
     license='BSD',
     description='Erppeek extension for using web services transactions',
     long_description='',
