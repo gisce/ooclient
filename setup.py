b9590b0f17ab605498915b6901430a6c165a4b37
Eduard Carreras <ecarreras@gisce.net>
2018-06-11 12:39:35 +0200

    Bump to v0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='ERPpeek WST',
-    version='0.2.1',
+    version='0.2.2',
     license='BSD',
     description='Erppeek extension for using web services transactions',
     long_description='',
